dd4064e08cd35e6e8ffc85ed20e182ea8a02e4a2
Alpcan Oenuer <alpcan.onur@gmail.com>
2022-10-07 19:24:09 +0200

    removed unwanted files and drew bouncing ball physics still missing

--- Object.py
+++ Object.py
@@ -1,17 +1,28 @@
 import pygame
-
+import math
 Black = (0,0,0)
 
 class Ball(pygame.sprite.Sprite):
     # This class represents a ball. It derives from the "Sprite" class in Pygame.
 
-    def __init__(self, color, width, height):
+    def __init__(self,color, x_coor,y_coor, radius,screen):
         # Call the parent class (Sprite) constructor
         super().__init__()
 
-        self.image = pygame.Surface([width, height])
+        self.image = pygame.Surface([x_coor,y_coor])
         self.image.fill(Black)
         self.image.set_colorkey(Black)
-        #Draw the ball (we are drawing a rectangle!)
-        pygame.draw.rect(self.image, color, [0, 0, width, height])
+        #Draw the ball
+        pygame.draw.circle(self.image, color, [x_coor//2, y_coor//2],radius)
         self.rect = self.image.get_rect()
+        self.screen = screen
+        self.rect.center = (self.screen.get_height()//2,0+radius)#Pos at time = 0
+        self.dy = 2*self.rect.centery-self.rect.centery+9.81*1/60
+
+
+
+    def update(self):
+        self.rect.centery += self.dy
+        if self.rect.bottom > self.screen.get_height() or self.rect.top < 0:
+            self.dy *= -1
+
--- PhysicsEngine.py
+++ PhysicsEngine.py
@@ -1,38 +1,35 @@
 import pygame
 from Object import Ball
+
 pygame.init()
-White = (255,255,255)
-Black = (0,0,0)
-(width,height) = (700,500)
-screen = pygame.display.set_mode((width,height))
-pygame.display.set_caption("Physics Engine")
-screen.fill(Black)
 
-ball = Ball(White,10,100)
-ball.rect.x = 345
-ball.rect.y = 195
-
-all_sprite_list = pygame.sprite.Group()
-all_sprite_list.add(ball)
-clock = pygame.time.Clock()
-running = True
-##Window Loop
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    all_sprite_list.update()
-    pygame.draw.line(screen, White, [349, 0], [349, 500], 5)
-    screen.fill(Black)
-    all_sprite_list.draw(screen)
-    pygame.display.flip()
-    clock.tick(60)
-
-pygame.quit()
+def main():
+    White = (255,255,255)
+    Black = (0,0,0)
+    (width,height) = (700,500)
+    screen = pygame.display.set_mode((width,height))
+    pygame.display.set_caption("Physics Engine")
+    background = pygame.Surface(screen.get_size())
+    background.fill(Black)
+    screen.blit(background,(0,0))
 
 
-
-
+    ball = Ball(White,20,20,10,screen)
+    all_sprite_list = pygame.sprite.Group()
+    all_sprite_list.add(ball)
+    clock = pygame.time.Clock()
+    running = True
+    ##Window Loop
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        all_sprite_list.clear(screen,background)
+        all_sprite_list.update()
+        all_sprite_list.draw(screen)
+        pygame.display.flip()
+        clock.tick(60)
 
 if __name__ == "__main__":
     main()
+pygame.quit()
