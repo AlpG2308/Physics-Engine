dea5be2b19e5b0873f01be3128c6d38719b3a861
Alpcan Oenuer <alpcan.onur@gmail.com>
2022-10-09 22:46:38 +0200

    velocity scaling works but bouncing has some porblems

--- Object.py
+++ Object.py
@@ -17,23 +17,24 @@ class Ball(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.screen = screen
         self.rect.center = (self.screen.get_width()//2,0+radius)#Pos at time = 0
-        self.prev_pos = [(self.screen.get_width()/2)-5,0+radius-5]
+        self.prev_pos = [self.screen.get_width()/2,0+radius-5]
 
 
     def update(self):
-        new_posy = 2 * self.rect.centery - self.prev_pos[1] + (9.81 * 0.5*0.5)
-        #new_posx = 2 * self.rect.center - self.prev_pos[0] + (0 * 0.2*0.2)
-        self.rect.centery += new_posy - self.prev_pos[1]
-        v_y = new_posy - self.prev_pos[0]
-        #self.rect.centerx += new_posx - self.prev_pos[0]
-        #self.prev_pos[0] = new_posx
+        new_posy = int(2 * self.rect.centery - self.prev_pos[1] + 9.81 * 0.5*0.5)
+        new_posx = int(2 * self.rect.centerx - self.prev_pos[0] + 0 * 0.5*0.5)
+        v_y = new_posy - self.prev_pos[1]
+        v_x = new_posx - self.prev_pos[0]
+        print(self.rect.centery, new_posy, self.prev_pos[1], v_y)
+        self.prev_pos[0] = new_posx
         self.prev_pos[1] = new_posy
-        if self.rect.centery >= self.screen.get_height():
+        self.rect.center = (new_posx,new_posy)
+        if self.rect.bottom > self.screen.get_height():
             new_posy = self.screen.get_height()
-            self.prev_pos[1] = new_posy + v_y * 0.9
+            self.rect.bottom= new_posy
+            self.prev_pos[1] = new_posy + v_y
+        if self.rect.top < 0:
+            new_posy = 0
+            self.prev_pos[1] = new_posy + v_y
 
-        elif self.rect.centery <= 0.0:
-            new_posy = 0.0
-            self.prev_pos[1] = new_posy + v_y * 0.9
-        print (v_y)
 
