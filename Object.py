7f9dabf92efa7cac6e517581886cc5d317026d50
Alpcan Oenuer <alpcan.onur@gmail.com>
2022-10-09 18:53:48 +0200

    Implemented Bounciness but velocity are not right

--- Object.py
+++ Object.py
@@ -17,21 +17,23 @@ class Ball(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.screen = screen
         self.rect.center = (self.screen.get_width()//2,0+radius)#Pos at time = 0
-        #self.dy = 2*self.rect.centery-self.rect.centery+9.81*1/60
-        self.prev_pos = [(self.screen.get_width()/2)-5,0+radius-10]
-        self.vel_y = 0
-        self.vel_x = 0
+        self.prev_pos = [(self.screen.get_width()/2)-5,0+radius-5]
+
 
     def update(self):
-        new_posy = 2 * self.rect.centery - self.prev_pos[1] + (1 * 0.2*0.2)
-        new_posx = 2 * self.rect.centerx - self.prev_pos[0] + (0 * 0.2*0.2)
-        self.vel_y = (new_posy + self.prev_pos[1])*0.5 * 0.2
-        self.vel_x = 0
-        self.prev_pos[0] = new_posx
+        new_posy = 2 * self.rect.centery - self.prev_pos[1] + (9.81 * 0.5*0.5)
+        #new_posx = 2 * self.rect.center - self.prev_pos[0] + (0 * 0.2*0.2)
+        self.rect.centery += new_posy - self.prev_pos[1]
+        v_y = new_posy - self.prev_pos[0]
+        #self.rect.centerx += new_posx - self.prev_pos[0]
+        #self.prev_pos[0] = new_posx
         self.prev_pos[1] = new_posy
-        self.rect.centery += self.vel_y
-        self.rect.centerx += self.vel_x
-        if self.rect.centery > self.screen.get_height():
+        if self.rect.centery >= self.screen.get_height():
             new_posy = self.screen.get_height()
-            self.prev_pos[1] = new_posy + self.vel_y * 0.9
-        print(self.vel_y)
+            self.prev_pos[1] = new_posy + v_y * 0.9
+
+        elif self.rect.centery <= 0.0:
+            new_posy = 0.0
+            self.prev_pos[1] = new_posy + v_y * 0.9
+        print (v_y)
+
